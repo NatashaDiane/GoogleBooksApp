a011a7e6057479dd5a4b037fc6a89a9d9332b175
NatashaDiane <45070030+NatashaDiane@users.noreply.github.com>
2021-02-16 23:54:15 +0100

    Update Constants.java

--- app/src/main/java/com/natasha/googlebooksapp/utils/Constants.java
+++ app/src/main/java/com/natasha/googlebooksapp/utils/Constants.java
@@ -2,5 +2,5 @@ package com.natasha.googlebooksapp.utils;
 
 public class Constants {
     public static final String URL_LEFT = "https://www.googleapis.com/books/v1/volumes?q=";
-    public static final String URL_RIGHT ="&key=AIzaSyCZRGWC_dnc_pgDHL_yfrdEWgyhS7kf8ug";
+    public static final String URL_RIGHT ="Your Google API Generated key here";
 }
